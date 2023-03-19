9999bcba6e2b5f59298e5c09611227e6811423ac
BeiGua <lovemc2021@163.com>
2023-03-19 19:05:58 +0800

    add test,resources,add copy_file

---
+++ src/main/java/org/eml/fox/files/PropertiesUtils.java
@@ -0,0 +1,78 @@
+package org.eml.fox.files;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * @author 北瓜sakura
+ * @date 2023/03/19
+ * @since 1.0
+ */
+public class PropertiesUtils {
+    /**
+     * @return 用户当前目录
+     */
+    public static String getDir() {
+        return System.getProperty("user.dir");
+    }
+
+    /**
+     * @param source
+     * @param target
+     * @return
+     */
+    public static void copy(@NotNull File source ,File target) {
+        if (source.isFile()){
+            copyFile(source , target);
+        }else {
+            target = new File(target , source.getName());
+            if (!target.exists()){
+                target.mkdirs();
+            }
+            File[] files = source.listFiles();
+            if (files != null){
+                for (int i=0;i<files.length;i++){
+                    copyFile(files[i],target);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param source
+     * @param target 目标
+     * @return
+     * @description 复制文件
+     */
+    private static void copyFile(@NotNull File source , @NotNull File target){
+        if (!target.exists()){
+            target.mkdirs();
+        }
+        File targets = new File(target , source.getName());
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(targets);
+            byte[] b = new byte[1024];
+            int len=0;
+            System.out.println("Start copying......");
+            while ((len=is.read(b)) != -1){
+                os.write(b,0,len);
+            }
+            System.out.println("Done copying!");
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if (Objects.nonNull(is)) is.close();
+                if (Objects.nonNull(os)) os.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,18 @@
+import org.eml.fox.files.PropertiesUtils;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void testDir() {
+        System.out.println(PropertiesUtils.getDir());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testCopy(){
+        File source = new File("src/main/resources/user.properties");
+        File target = new File(PropertiesUtils.getDir());
+        PropertiesUtils.copy(source,target);
+    }
+}
