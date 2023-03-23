5fe144def1fd0b7898b3e9cbe54ab6f084805571
linmu <linmu013@163.com>
2023-03-24 00:46:17 +0800

    Update JUI

--- src/main/java/org/eml/fox/JUI/LoginFrame.java
+++ src/main/java/org/eml/fox/JUI/LoginFrame.java
@@ -7,16 +7,20 @@ import javax.swing.*;
 public class LoginFrame extends JFrame {
     public LoginFrame(){
         this.setVisible(true);
-        this.setSize(600,600);
+        this.setSize(500,500);
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setTitle("登录");
+        this.setLayout(null);
         //按钮
-        JButton jbutton = new JButton();
+        JButton jbutton = new JButton("登录微软账户");
+        JButton jbutton2 = new JButton("登录Mojang账户");
         //设置位置和宽高
-        jbutton.setBounds(30,10,10,3);
+        jbutton.setBounds(175,125,150,25);
+        jbutton2.setBounds(175,175,150,25);
         jbutton.addActionListener(e -> {
         });
         this.getContentPane().add(jbutton);
+        this.getContentPane().add(jbutton2);
     }
 }
