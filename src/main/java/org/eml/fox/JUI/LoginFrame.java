51550364dd185f2920554d335c05a6ee90847c00
xiaoxing365 <645710596@qq.com>
2023-03-24 00:17:38 +0800

    Fixed ClassCast

--- src/main/java/org/eml/fox/JUI/LoginFrame.java
+++ src/main/java/org/eml/fox/JUI/LoginFrame.java
@@ -1,11 +1,8 @@
 package org.eml.fox.JUI;
 
 
-//import org.eml.fox.JUI.Listeners.TheActionListener;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class LoginFrame extends JFrame {
     public LoginFrame(){
@@ -16,7 +13,6 @@ public class LoginFrame extends JFrame {
         this.setTitle("登录");
         //按钮
         JButton jbutton = new JButton();
-        jbutton.setIcon((Icon) IniPictures.getmsLogin());
         //设置位置和宽高
         jbutton.setBounds(30,10,10,3);
         jbutton.addActionListener(e -> {
