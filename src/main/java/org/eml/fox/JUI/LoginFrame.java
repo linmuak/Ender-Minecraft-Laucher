fa70f6bcd2b9f32be43b663ff08847d57d0a5b08
linmu <linmu013@163.com>
2023-03-25 21:56:47 +0800

    Update JUI New JButton:jbutton3 (其它登录)

--- src/main/java/org/eml/fox/JUI/LoginFrame.java
+++ src/main/java/org/eml/fox/JUI/LoginFrame.java
@@ -13,11 +13,14 @@ public class LoginFrame extends JFrame {
         //按钮
         JButton jbutton = new JButton("登录微软账户");
         JButton jbutton2 = new JButton("登录Mojang账户");
+        JButton jbutton3 = new JButton("其它登录");
         jbutton.setBounds(175,125,150,25);
         jbutton2.setBounds(175,175,150,25);
+        jbutton3.setBounds(175,225,150,25);
         jbutton.addActionListener(e -> {
         });
         this.getContentPane().add(jbutton);
         this.getContentPane().add(jbutton2);
+        this.getContentPane().add(jbutton3);
     }
 }
